9f12f6df1b0909a9ccac1339ccd2dcdd445c94f5
venuziano <raf_rs@hotmail.com>
2020-05-30 14:22:34 -0300

    css

--- front-tcc/src/pages/RegisterTransaction/index.js
+++ front-tcc/src/pages/RegisterTransaction/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import './styles.css'
 import logoImg from '../../assets/logo.png';
+import loadImg from '../../assets/loading.gif'
 import api from '../../services/api'
 
 export default function RegisterTransaction() {
@@ -12,6 +13,9 @@ export default function RegisterTransaction() {
   const [tx, setTx] = useState('');
   const [receipt, setReceipt] = useState('');
   const [showReceipt, setShowReceipt] = useState(false);
+  const realFile = React.createRef();
+  const customText = React.createRef();
+
   
   async function handleRegister() {
 
@@ -57,7 +61,7 @@ export default function RegisterTransaction() {
     setShowReceipt(true);
   }
 
-  const Results = () => (
+  const ShowResults = () => (
     <div id="receipt">
       <label>Comprovante do registro:</label>
       <input 
@@ -66,6 +70,27 @@ export default function RegisterTransaction() {
       <p>Consulte seu comprovante : <a href={receipt}>aqui.</a></p>
     </div>
   )
+
+  function showLoading() {
+    const Loading = () => (
+      <div id="loading" className="load">
+        <img alt="caregando" src={loadImg}></img>
+        <span>Registrando...</span>
+      </div>
+    )
+  }
+
+  function focusTextInput() {
+    realFile.current.click();
+  }
+
+  function validatePatch() {
+    if(realFile.current.value) {
+      customText.current.innerHTML = realFile.current.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    }else {
+      customText.current.innerHTML = "Nenhum documento selecionado."
+    }
+  }
 
   return (
     <div className="register-container">
@@ -80,12 +105,15 @@ export default function RegisterTransaction() {
         </section>
 
         <div className="form">
-          <label>Documento: </label>
-          <input className="upload-button"
+          <input
+            hidden="hidden"
             name="file"
             type="file"
-            onChange={e => setDocument(e.target.files[0])}
+            ref={realFile}
+            onChange={e => { setDocument(e.target.files[0]); validatePatch() }}
           />
+          <button type="button"  className="upload-button" onClick={focusTextInput}>Selecione um documento: </button>
+          <span ref={customText} className="custom-text">Nenhum documento selecionado.</span>
 
           <button className="button" type="submit" onClick={onFileUpload}>Upload</button> 
       
@@ -105,8 +133,8 @@ export default function RegisterTransaction() {
           />
 
           <button className="button" type="submit" onClick={() => { handleRegister(); showReceiptDiv() }}>Registrar</button>
-          
-          {showReceipt ? <Results /> : null}
+
+          {showReceipt ? <ShowResults /> : null}
         </div>
       </div>
     </div>
